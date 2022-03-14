46887dc2eb83a7c62f7378c7116cdee0f61ba57f
SergiCuen <sergio@dtic.ua.es>
2022-03-14 23:36:12 +0100

    modificado minicom.sh

--- minicom.sh
+++ minicom.sh
@@ -1,5 +1,5 @@
-# !/bin/bash
-# Conexión a puerto serial debug 
-# uso: . minicom.sh datenamefile.log
+# Minicom session connected to TX2 serial debug
+# to include timestamp Ctrl+A => Z => N:wq
+# usage: . minicom.sh datenamefile.log
 
 minicom -D /dev/ttyUSB0 -b 115200 -C $1
